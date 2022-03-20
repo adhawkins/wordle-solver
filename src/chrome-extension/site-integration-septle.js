690c7725053c93e1a3ffb4d10ae0729477755661
Andy Hawkins <andy@gently.org.uk>
2022-03-20 23:17:56 +0000

    Add septle site integration

--- src/chrome-extension/site-integration-septle.js
+++ src/chrome-extension/site-integration-septle.js
@@ -1,8 +1,35 @@
+const septleMutationClasses = [];
+
 class CSiteIntegrationSeptle extends CSiteIntegration {
 	constructor(solverInterface) {
 		super(solverInterface);
 
-		this.wordLength = 7;
+		this.observer = new MutationObserver(CSiteIntegrationSeptle.changesCallback);
+		wordleMutationClasses.push({
+			observer: this.observer,
+			integration: this
+		});
+
+		const integration = this;
+
+		const board = document.querySelectorAll('table.squares')[0];
+		if (board) {
+			const rows = board.querySelectorAll('tr');
+			if (rows) {
+				this.lastRow = rows[rows.length - 1];
+				const integration = this;
+				rows.forEach((function (row) {
+					const tiles = row.querySelectorAll('td');
+					integration.wordLength = tiles.length;
+					integration.observer.observe(tiles[tiles.length - 1], { attributes: true });
+				}));
+			}
+		}
+
+		const alertBox = document.querySelectorAll('#alertBox');
+		if (alertBox) {
+			this.observer.observe(alertBox[0], { childList: true });
+		}
 	}
 
 	sendKey(key) {
@@ -30,5 +57,65 @@ class CSiteIntegrationSeptle extends CSiteIntegration {
 		document.dispatchEvent(new KeyboardEvent('keydown', params));
 		document.dispatchEvent(new KeyboardEvent('keyup', params));
 
+	}
+
+	static changesCallback(changes, observer) {
+		let integration = null;
+		const changesMap = new Map();
+
+		for (const mutationClass of wordleMutationClasses) {
+			if (mutationClass.observer = observer) {
+				integration = mutationClass.integration;
+			}
+		}
+
+		for (const change of changes) {
+			switch (change.type) {
+				case 'attributes':
+					if (change.attributeName == "data-state") {
+						const newState = change.target.getAttribute(change.attributeName);
+						changesMap.set(change.target, newState);
+					}
+
+					break;
+
+				case 'childList':
+					for (const node of change.addedNodes) {
+						if (node.nodeName == 'SPAN' && node.textContent == 'Not in word list!') {
+							integration.solverInterface.badGuess();
+						}
+					}
+
+					break;
+			}
+		}
+
+		for (const [target, newState] of changesMap) {
+			if (newState && newState != "tbd") {
+				const parent = target.parentElement;
+				const tiles = parent.querySelectorAll('td');
+
+				let result = ' '.repeat(integration.wordLength);
+
+				for (let count = 0; count < tiles.length; count++) {
+					switch (tiles[count].getAttribute('data-state')) {
+						case 'absent':
+							result = CSiteIntegration.setCharAt(result, count, '0');
+							break;
+
+						case 'present':
+							result = CSiteIntegration.setCharAt(result, count, '1');
+							break;
+
+						case 'correct':
+							result = CSiteIntegration.setCharAt(result, count, '2');
+							break;
+					}
+				}
+
+				const row = parent;
+				integration.solverInterface.guessMarked(result, row == integration.lastRow);
+			}
+		}
 	}
 }
